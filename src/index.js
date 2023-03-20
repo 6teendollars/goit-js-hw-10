4c9a57cc1763e52016ef6d8eb190842d00409232
6teendollars <91662992+6teendollars@users.noreply.github.com>
2023-03-20 21:59:17 +0100

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,65 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+const ref = {
+  search: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+};
+
+ref.search.addEventListener('input', debounce(searchCountry, DEBOUNCE_DELAY));
+
+function searchCountry(e) {
+  const search = e.target.value.trim();
+  if (search !== '') {
+    fetchCountries(search)
+      .then(renderMarkup)
+      .catch(error => console.log(error));
+  }
+
+  function renderMarkup(countries) {
+    ref.countryList.innerHTML = '';
+    ref.countryInfo.innerHTML = '';
+
+    if (countries.length > 10) {
+      Notiflix.Notify.info(
+        'Too many matches found. Please enter a more specific name.'
+      );
+      return;
+    }
+
+    if (countries.length > 2 && countries.length < 10) {
+      const markcupListCountries = countries
+        .map(
+          ({ name, flags }) =>
+            `<li class='country'>
+            <img class='country-flag' alt="${name.official}" src='${flags.svg}' width=30>
+            <span class='country-name'>${name.official}</span></li>`
+        )
+        .join('');
+
+      ref.countryList.innerHTML = markcupListCountries;
+      return;
+    }
+
+    if (countries.length === 1) {
+      const [{ name, flags, capital, population, languages }] = countries;
+      ref.countryInfo.innerHTML = `<div class='container'>
+                <img class='country-flag' alt=${name.official} src='${
+        flags.svg
+      }' width=70 height=50>
+                <h1 class='country-title'>${name.official}</h1>
+            </div>
+            <p><span class='title-info'>Capital:</span> ${capital}</p>
+            <p><span class='title-info'>Population:</span> ${population}</p>
+            <p><span class='title-info'>Languages:</span> ${Object.values(
+              languages
+            ).join('')}</p>`;
+      return;
+    }
+  }
+}
